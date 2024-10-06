552e21ccd47eeb84ddd1e08b847778509d2f2ee3
Pruszyn5112 <pruszynbusiness@gmail.com>
2024-10-06 20:59:53 +0200

    Add deck

--- BlackJack/Card.cs
+++ BlackJack/Card.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlackJack
+﻿namespace BlackJack
 {
     internal class Card
     {
+        public string Suit { get; set; }
+        public string Rank { get; set; }
+        public int Value { get; set; }
+
+        public Card(string suit, string rank, int value)
+        {
+            Suit = suit;
+            Rank = rank;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
     }
 }
--- BlackJack/Deck.cs
+++ BlackJack/Deck.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlackJack
+﻿namespace BlackJack
 {
-    internal class Deck
+    public class Deck
     {
+        private List<Card> _cards;
+
+        public Deck()
+        {
+            _cards = new List<Card>();
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            int[] values = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11 };
+
+            foreach (var suit in suits)
+            {
+                for (int i = 0; i < ranks.Length; i++)
+                {
+                    _cards.Add(new Card(suit, ranks[i], values[i]));
+                }
+            }
+
+            Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            Random randomize = new Random();
+            _cards = _cards.OrderBy(c => randomize.Next()).ToList();
+        }
     }
 }
+
